7a09370ddccb6fb7df8aa84261e374fd9f6e27b2
Jan Bujak <j@exia.io>
2022-02-26 06:15:49 +0000

    Also try to load dl symbols from libc

--- src/dl.rs
+++ src/dl.rs
@@ -8,11 +8,21 @@ extern {
 }
 
 pub unsafe fn initialize_dlsym() -> *mut c_void {
-    hooky_load_symbol( b"*libdl.so*\0".as_ptr() as *const c_char, b"dlsym\0".as_ptr() as *const c_char )
+    let mut p = hooky_load_symbol( b"*libdl.so*\0".as_ptr() as *const c_char, b"dlsym\0".as_ptr() as *const c_char );
+    if p.is_null() {
+        p = hooky_load_symbol( b"*libc.so*\0".as_ptr() as *const c_char, b"dlsym\0".as_ptr() as *const c_char );
+    }
+
+    p
 }
 
 pub unsafe fn initialize_dlvsym() -> *mut c_void {
-    hooky_load_symbol( b"*libdl.so*\0".as_ptr() as *const c_char, b"dlvsym\0".as_ptr() as *const c_char )
+    let mut p = hooky_load_symbol( b"*libdl.so*\0".as_ptr() as *const c_char, b"dlvsym\0".as_ptr() as *const c_char );
+    if p.is_null() {
+        p = hooky_load_symbol( b"*libc.so*\0".as_ptr() as *const c_char, b"dlvsym\0".as_ptr() as *const c_char );
+    }
+
+    p
 }
 
 pub unsafe fn initialize_libc_dlsym() -> *mut c_void {
